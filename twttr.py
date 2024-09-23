2a43265c2501a8baeffd66ed660a01e5dc64e9c1
duanniten <duann_@hotmail.com>
2024-09-23 17:15:57 -0300

    change twttr implemetation

---
+++ twttr.py
@@ -0,0 +1,30 @@
+def main():
+    text = input("Input: ")
+    
+    new_text = creat_text(text)
+    
+    print("Output:", new_text)
+
+def shorten(word: str):
+    vowels = [
+            'A', 'a',
+            'E', 'e',
+            'I', 'i',
+            'O', 'o', 
+            'U', 'u'
+            ]
+    new_word = ""
+    for lether in word:
+        if lether not in vowels:
+            new_word += lether
+    return new_word
+
+def creat_text(text: str): 
+    text = text.split(sep = ' ')
+    new_text = ''
+    for word in text:
+        new_text += f'{shorten(word)} '
+    return new_text[:-1]
+
+if __name__ == '__main__':
+    main()
